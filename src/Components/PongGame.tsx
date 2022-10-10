1ed86fe0adeb7bbd0aa88c5291a93738225b3b63
Pierre-louis Deshayes <pdeshaye@z2r2p7.42lyon.fr>
2022-10-10 19:36:09 +0200

    typeGane

--- src/Components/PongGame.tsx
+++ src/Components/PongGame.tsx
@@ -1,11 +1,12 @@
+import { homedir } from 'os'
 import React, { useEffect, useRef, useCallback } from 'react'
 import "../css/Components/PongGame.css"
 
-export default function PongGame({width, height} : {width: number, height: number}) {
+export default function PongGame({width, height, gameType} : {width: number, height: number, gameType: number}) {
     
     const canvasRef = useRef<HTMLCanvasElement>(null)
 
-    interface playerProps {
+        interface playerProps {
         x: number,
         y: number,
         height: number,
@@ -17,9 +18,9 @@ export default function PongGame({width, height} : {width: number, height: numbe
 
     const user1 =
     {
-        x: 15,
-        y: height / 2 - 60 / 2,
-        height: 60,
+        x: 50,
+        y: height / 2 - 200 / 2,
+        height: 200,
         width: 10,
         score: 0,
         color: "WHITE", 
@@ -28,13 +29,13 @@ export default function PongGame({width, height} : {width: number, height: numbe
 
     const user2 =
     {
-        x: width - 25,
+        x: width - 10 - 50,
         y: height/2 - 60/2,
         height: 60,
         width: 10,
         score: 0,
         color: "WHITE",
-        speed: 10
+        speed: 30
     }
 
     const midLine = 
@@ -144,12 +145,15 @@ export default function PongGame({width, height} : {width: number, height: numbe
     {
         let colidePoint = 0;
         let angleCollision = 0;
-        const computer_level = 0.25
-        let botMove = (ball.y - (user2.y + user2.height / 2)) * computer_level
+        const computer_level = 0.8
+        if (gameType == 3)
+        {
+            let botMove = (ball.y - (user2.y + user2.height / 2)) * computer_level
 
-        if (botMove - user2.height < height && botMove + user2.height > 0)
-            user2.y += botMove
-        
+            if (botMove - user2.height < height && botMove + user2.height > 0)
+                user2.y += botMove
+        }
+
         ball.x += ball.velocityX
         ball.y += ball.velocityY
         if (ball.y + ball.r >= height || ball.y - ball.r <= 0)
@@ -160,7 +164,7 @@ export default function PongGame({width, height} : {width: number, height: numbe
             angleCollision = colidePoint * (Math.PI / 4)
             ball.velocityX = ball.speed * Math.cos(angleCollision)
             ball.velocityY = ball.speed * Math.sin(angleCollision)
-            ball.speed += 0.5
+            ball.speed += 2
         }
         else if (player_collision(user2))
         {
@@ -168,7 +172,7 @@ export default function PongGame({width, height} : {width: number, height: numbe
             angleCollision = colidePoint * (Math.PI / 4)
             ball.velocityX = -ball.speed * Math.cos(angleCollision)
             ball.velocityY = ball.speed * Math.sin(angleCollision)
-            ball.speed += 0.5
+            ball.speed += 2
         }
         else if (ball.x + ball.r >= width || ball.x - ball.r <= 0)
         {
@@ -193,10 +197,13 @@ export default function PongGame({width, height} : {width: number, height: numbe
     const mouseMouveEvent = useCallback((e : MouseEvent) => 
     {
         if (canvasRef.current)
-        { 
+        {  
             let userRect = canvasRef.current.getBoundingClientRect() 
-            if (e.clientY - userRect.top - user1.height / 2 > 10 && e.clientY - userRect.top + user1.height / 2 < height - 10)  
-                user1.y = e.clientY - userRect.top - (user1.height / 2)
+            console.log("bizzar " , e.clientY - userRect.top, " / height ", height, "real canvas height : ", height / canvasRef.current.getBoundingClientRect().height)
+            if (e.clientY - userRect.top - user1.height / 2 > 10 && (e.clientY - userRect.top + user1.height / 2) < height - 10)  
+            {
+                user1.y = e.clientY - userRect.top - (user1.height / 2) 
+            }
             else if (e.clientY - userRect.top - user1.height / 2 > 10)
                 user1.y = height - user1.height - 10
             else 
@@ -204,31 +211,50 @@ export default function PongGame({width, height} : {width: number, height: numbe
         }
     }, []);
 
+    const touchStartLister = useCallback((e : TouchEvent) => 
+    {
+        if (canvasRef.current)
+        { 
+            let newPos = e.touches[0].clientY
+            let userRect = canvasRef.current.getBoundingClientRect() 
+            if (newPos  - userRect.top - user1.height / 2 > 10 && newPos - userRect.top + user1.height / 2 < height - 10)  
+                user1.y = newPos - userRect.top - (user1.height / 2)
+            else if (newPos - userRect.top - user1.height / 2 > 10)
+                user1.y = height - user1.height - 10
+            else 
+                user1.y = 10
+        }
+    }, []);
 
-    // const handleUserKeyPress = useCallback((e : KeyEvent) => 
-    // {
 
-    //     if (e.key == "ArrowUp")
-    //     {
-    //         if (user1.y > 0)
-    //             user1.y -= user1.speed; 
-    //     }
-    //     else if (e.key == "ArrowDown")
-    //     {
-    //         if (user1.y + user1.height < height) 
-    //             user1.y += user1.speed; 
-    //     }
-    // }, []);
+
+    const handleUserKeyPress = useCallback((e : KeyboardEvent) => 
+    {
+        if (e.key == "ArrowUp")
+        {
+            if (user2.y > 0)
+                user2.y -= user2.speed; 
+        }
+        else if (e.key == "ArrowDown")
+        {
+            if (user2.y + user1.height < height) 
+                user2.y += user2.speed;
+        }
+    }, []);
 
     useEffect(()=>{
+        console.log(gameType)
         if (canvasRef.current)
         {
             const canvas = canvasRef.current
             const context = canvas.getContext('2d')
             window.addEventListener("mousemove", mouseMouveEvent);
+            window.addEventListener('touchmove', touchStartLister);
+            if (gameType == 1)
+                window.addEventListener('keydown', handleUserKeyPress);
             if (context)
             {
-                const frame = 50;
+                const frame = 60;
                 const interval = setInterval(() => {
                     game(context) 
                 }, 1000 / frame)
@@ -237,12 +263,15 @@ export default function PongGame({width, height} : {width: number, height: numbe
                 {
                     clearInterval(interval)  
                     window.removeEventListener("mousemove", mouseMouveEvent);
+                    window.removeEventListener("touchmove", touchStartLister);
+                    if (gameType == 1)
+                        window.addEventListener('keydown', handleUserKeyPress);
                 }
             }
         }
     }, [])
 
   return (
-        <canvas ref={canvasRef} width={width} height={height}/>
+        <canvas className='canvasGame' ref={canvasRef} width={width} height={height}/>
         )
 }
--- src/Pages/GamePage/MainGame.tsx
+++ src/Pages/GamePage/MainGame.tsx
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PongGame from '../../Components/PongGame';
 import "../../css/Pages/MainGame.css"
 
 export default function MainGame () 
 {
+    const [gameType, setGameType] = useState<number>(1)
+
+    useEffect(() =>{
+
+    }, [gameType])
+
     return (
-        <div className='mainGame_block'>
+        <div className='mainGame_block' key={gameType}>
             <h1 className='mainGame_GameTitle'> Game Page </h1>
-            <PongGame width={1000} height={600}/>
+            <div className='mainGame_selectTypeGame'>
+                <button type="button" className="btn btn-outline-danger" onClick={() => setGameType(1)}>Local 1v1</button>
+                <button type="button" className="btn btn-outline-danger" onClick={() => setGameType(3)}>Bot</button>
+                <button type="button" className="btn btn-outline-danger" onClick={() => setGameType(2)}>Online</button>
+            </div>
+
+            <div  className='mainGame_blockGame'>
+                <PongGame width={1500} height={800} gameType={gameType}/>
+            </div>
+        
         </div>
     )
 }
